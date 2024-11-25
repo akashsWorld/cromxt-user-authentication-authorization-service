7c351e1ab14ff02ac213e0b3ec8a5e210f6b883c
Akash Biswas <akashbiswas.271999@gmail.com>
2024-11-25 13:56:34 +0530

    Change the request verb to Get for the validate endpoint.

--- user-service/src/main/java/com/cromxt/user/controller/ValidationController.java
+++ user-service/src/main/java/com/cromxt/user/controller/ValidationController.java
@@ -1,15 +1,14 @@
 package com.cromxt.user.controller;
 
 
-import com.cromxt.user.dtos.requests.UserCredential;
 import com.cromxt.user.dtos.responses.UserResponse;
-import com.cromxt.user.exceptions.UnauthorizedAPIKeyException;
 import com.cromxt.user.service.AuthorizationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/service")
@@ -18,8 +17,8 @@ public class ValidationController{
 
     private final AuthorizationService authorizationService;
 
-    @PostMapping(value="{username}")
-    public ResponseEntity<UserResponse> getUserDetails(@PathVariable String username) throws UnauthorizedAPIKeyException {
+    @GetMapping(value="{username}")
+    public ResponseEntity<UserResponse> getUserDetails(@PathVariable String username) {
         return ResponseEntity.ok(authorizationService.getUserDetails(username));
     }
 }
