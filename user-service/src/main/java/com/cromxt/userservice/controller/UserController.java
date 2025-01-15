a139e9d70d453d0a411df015961057053304b939
Akash Biswas <akashbiswas.271999@gmail.com>
2025-01-15 10:17:15 +0530

    Create the sign-up user page.

--- user-service/src/main/java/com/cromxt/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cromxt/userservice/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.cromxt.userservice.controller;
 
 
-import com.cromxt.userservice.dtos.requests.NewUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = {"/accounts","/"})
 public class UserController {
 
+
+
     @GetMapping(value = {"/login","/"})
-    public String login(@RequestParam(name = "continueTo", required = false) String continueTo) {
-        System.out.println(continueTo);
+    public String login(
+            @RequestParam(name = "continueTo", required = false) String continueTo,
+            Model model) {
+        String baseUrl = System.getProperty("USER_SERVICE_BASE_URL");
+        model.addAttribute("redirectUrl", continueTo);
+        model.addAttribute("baseUrl",baseUrl);
         return "login";
     }
     @GetMapping("/register")
--- user-service/src/main/resources/static/js/script.js
+++ user-service/src/main/resources/static/js/script.js
@@ -9,3 +9,40 @@ $('#toggle-password').click(function () {
         $(this).find('i').removeClass('fa-eye-slash').addClass('fa-eye');
     }
 });
+
+$('#avatar').on('change', function () {
+    const file = this.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const preview = document.getElementById('avatar-preview');
+        const cameraIcon = document.getElementById('camera-icon');
+        cameraIcon.style.display = 'none';
+        preview.style.backgroundImage = `url(${e.target.result})`;
+    };
+
+    reader.readAsDataURL(file);
+})
+$('#toggle-confirm-password').click(function () {
+    const passwordField = $('#confirm-password');
+    const passwordFieldType = passwordField.attr('type');
+    if (passwordFieldType === 'password') {
+        passwordField.attr('type', 'text');
+        $(this).find('i').removeClass('fa-eye').addClass('fa-eye-slash');
+    } else {
+        passwordField.attr('type', 'password');
+    }
+});
+
+$('register-form').on('submit',(form)=>{
+    form.preventDefault();
+    const password = $('#password').val();
+    const confirmPassword = $('#confirm-password').val();
+    console.log("password: "+password+" confirmPassword: "+confirmPassword);
+    if(password !== confirmPassword){
+        $('password_input').style.border = '1px solid red';
+        $('confirm_password_input').style.border = '1px solid red';
+    }else {
+        form.submit();
+    }
+})
+
